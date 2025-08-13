6e19cb6e6f08bae077c20dd766f336dd4f6c0185
Oliwia Pawlak <oliwiap245@gmail.com>
2025-08-13 12:06:58 +0200

    initial commit

---
+++ MoviesApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MoviesApp
+//
+//  Created by oliwia on 07/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+     Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by oliwia on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
